fc88337a8bd27c780372e9479fb0313ff8e48882
hoaphumanoid <miguelgfierro@gmail.com>
2016-11-27 09:50:06 +0000

    trying to fix github bug with notebooks

--- Visualization_of_Football_Matches/utils.py
+++ Visualization_of_Football_Matches/utils.py
@@ -56,3 +56,32 @@ country_dict = {'ALB':'Albania',
                 #'WAL':'Wales',
                 'WAL':'United Kingdom'}
 
+
+def to_web_mercator(yLat, xLon):
+    # Check if coordinate out of range for Latitude/Longitude
+    if (abs(xLon) > 180) and (abs(yLat) > 90):  
+        return
+ 
+    semimajorAxis = 6378137.0  # WGS84 spheriod semimajor axis
+    east = xLon * 0.017453292519943295
+    north = yLat * 0.017453292519943295
+ 
+    northing = 3189068.5 * math.log((1.0 + math.sin(north)) / (1.0 - math.sin(north)))
+    easting = semimajorAxis * east
+ 
+    return [easting, northing]
+
+def aggregate_dataframe_coordinates(dataframe):
+    df = pd.DataFrame(index=np.arange(0, n_complete_matches*3), columns=['Latitude','Longitude'])
+    count = 0
+    for ii in range(dataframe.shape[0]):
+        if dataframe['home_stadium'].loc[ii]!= 'Unknown' and dataframe['visitor_stadium'].loc[ii]!= 'Unknown':
+            df.loc[count] = [dataframe['home_latitude'].loc[ii], dataframe['home_longitude'].loc[ii]]
+            df.loc[count+1] = [dataframe['visitor_latitude'].loc[ii], dataframe['visitor_longitude'].loc[ii]]
+            df.loc[count+2] = [np.NaN, np.NaN]
+            count += 3
+    return df
+
+
+
+
