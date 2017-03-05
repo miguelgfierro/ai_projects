da6affe2bb6054b9a429de52c87ef8f18b7d8cd5
hoaphumanoid <miguelgfierro@gmail.com>
2017-03-05 12:11:00 +0000

    working on the API

---
+++ Intro_to_Machine_Learning_API/api.py
@@ -0,0 +1,57 @@
+from flask import Flask
+from flask import request, abort, jsonify, make_response, json
+from cntk import load_model, combine
+
+
+BAD_REQUEST = 400
+app = Flask(__name__)
+
+
+@app.errorhandler(BAD_REQUEST)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), BAD_REQUEST)
+
+
+@app.route('/api/v1/classify_image', methods=['POST'])
+def post_status():
+    if not request.json or 'image' not in request.json:
+        abort(BAD_REQUEST)
+    image_request = request.json['image']
+    img = read_image_from_request(image_request)
+    resp = predict(NULL, img, NULL, 1)
+    return make_response(jsonify({'message': resp}), STATUS_OK)
+
+
+def read_image_from_request(image_request):
+    img = Image.open(BytesIO(image_request.read())).convert('RGB')
+    return img
+
+        
+def read_synsets(filename='synsets.txt'):
+    with open(filename, 'r') as f:
+        synsets = [l.rstrip() for l in f]
+        labels = [" ".join(l.split(" ")[1:]) for l in synsets]
+    return labels
+
+
+def predict(model, image, labels, number_results = 5):
+    #FIXME: load this with the API
+    model = load_model('ResNet_18.model')
+    labels = read_synsets()
+
+    #Crop and center the image
+    img = ImageOps.fit(image, (224, 224), Image.ANTIALIAS)
+    #Transform the image for CNTK format
+    img = np.array(img, dtype=np.float32)
+    img = np.ascontiguousarray(np.transpose(img, (2, 0, 1)))
+    # Use last layer to make prediction
+    z_out = combine([model.outputs[3].owner])
+    result = np.squeeze(z_out.eval({z_out.arguments[0]:[img]}))
+    # Sort probabilities 
+    prob_idx = np.argsort(result)[::-1][:number_results]
+    pred = [labels[i] for i in prob_idx]
+    return pred
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
