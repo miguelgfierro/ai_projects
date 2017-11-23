5698872d9397bb7384b2b4b22c6e91877b9c897f
miguelgfierro <miguelgfierro@users.noreply.github.com>
2017-11-23 07:50:48 +0000

    train verbose

--- A_Gentle_Introduction_to_Transfer_Learning/utils.py
+++ A_Gentle_Introduction_to_Transfer_Learning/utils.py
@@ -119,7 +119,7 @@ def plot_pytorch_data_stream(dataobject, max_images=8, title=True):
         plt.title(names)
 
 
-def train_model(dataloaders, model, criterion, optimizer, scheduler, num_epochs=25):
+def train_model(dataloaders, model, criterion, optimizer, scheduler, num_epochs=25, verbose=True):
     since = time.time()
     sets = list(dataloaders.keys())
     dataset_sizes = {x: len(dataloaders[x].dataset) for x in sets}
@@ -128,8 +128,9 @@ def train_model(dataloaders, model, criterion, optimizer, scheduler, num_epochs=
     acc_per_epoch = []
     loss_per_epoch = []
     for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch + 1, num_epochs))
-        print('-' * 10)
+        if verbose:
+            print('Epoch {}/{}'.format(epoch + 1, num_epochs))
+            print('-' * 10)
 
         # Each epoch has a training and validation phase
         for phase in sets:
@@ -171,7 +172,7 @@ def train_model(dataloaders, model, criterion, optimizer, scheduler, num_epochs=
 
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects / dataset_sizes[phase]
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+            if verbose: print('{} Loss: {:.4f} Acc: {:.4f}\n'.format(phase, epoch_loss, epoch_acc))
             if phase == 'val':
                 loss_per_epoch.append(epoch_loss)
                 acc_per_epoch.append(epoch_acc)
@@ -181,12 +182,11 @@ def train_model(dataloaders, model, criterion, optimizer, scheduler, num_epochs=
                 best_acc = epoch_acc
                 best_model_wts = model.state_dict()
 
-        print()
 
     time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
+    if verbose:
+        print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+        print('Best val Acc: {:4f}'.format(best_acc))
 
     # load best model weights
     model.load_state_dict(best_model_wts)
