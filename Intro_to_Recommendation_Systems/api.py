fdd02b341a1a0f2b6440ce33bc526cf75b8aac31
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-01-22 23:57:35 +0000

    api recommender unfinished

--- Intro_to_Recommendation_Systems/api.py
+++ Intro_to_Recommendation_Systems/api.py
@@ -1,11 +1,38 @@
-from flask import render_template
 import cherrypy
 from paste.translogger import TransLogger
-from flask import Flask
+from flask import Flask, request, abort, jsonify, make_response
+import json
+import os
+from utils import decode_string
+from DeepRecommender.reco_encoder.data import input_layer_api, input_layer
+from DeepRecommender.reco_encoder.model import model
+import torch
+from torch.autograd import Variable 
+from parameters import *
 
 
 # app
 app = Flask(__name__)
+BAD_REQUEST = 400
+STATUS_OK = 200
+NOT_FOUND = 404
+SERVER_ERROR = 500
+
+
+@app.errorhandler(BAD_REQUEST)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), BAD_REQUEST)
+
+
+@app.errorhandler(NOT_FOUND)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), NOT_FOUND)
+
+
+@app.errorhandler(SERVER_ERROR)
+def server_error(error):
+    return make_response(jsonify({'error': 'Server Internal Error'}), SERVER_ERROR)
+
 
 def run_server():
     # Enable WSGI access logging via Paste
@@ -29,9 +56,91 @@ def run_server():
     cherrypy.engine.block()
 
 
+def load_model_weights(model_architecture, weights_path):
+    if os.path.isfile(weights_path):
+        print("Loading model from: {}".format(weights_path))
+        model_architecture.load_state_dict(torch.load(weights_path))
+    else:
+        raise ValueError("Path not found {}".format(weights_path))
+
+        
+def load_recommender(vector_dim, hidden, activation, dropout, weights_path):
+
+    rencoder_api = model.AutoEncoder(layer_sizes=[vector_dim] + [int(l) for l in hidden.split(',')],
+                               nl_type=activation,
+                               is_constrained=False,
+                               dp_drop_prob=dropout,
+                               last_layer_activations=False)
+    load_model_weights(rencoder_api, weights_path) 
+    rencoder_api.eval()
+    rencoder_api = rencoder_api.cuda()
+    return rencoder_api
+
+        
+def load_train_data(data_dir):    
+    params = dict()
+    params['batch_size'] = 1
+    params['data_dir'] =  data_dir
+    params['major'] = 'users'
+    params['itemIdInd'] = 1
+    params['userIdInd'] = 0
+    print("Loading training data")
+    data_layer = input_layer.UserItemRecDataProvider(params=params)
+    return data_layer
+
+
+def manage_query(dict_query, data_layer):
+    params = dict()
+    params['batch_size'] = 1
+    params['data_dict'] =  dict_query
+    params['major'] = 'users'
+    params['itemIdInd'] = 1
+    params['userIdInd'] = 0
+    data_api = input_layer_api.UserItemRecDataProviderAPI(params=params,
+                                                        user_id_map=data_layer.userIdMap,
+                                                        item_id_map=data_layer.itemIdMap)
+    data_api.src_data = data_layer.data
+    return data_api
+
+
+def evaluate_model(rencoder_api, data_api, data_layer):   
+    #inv_userIdMap = {v: k for k, v in data_api.userIdMap.items()}
+    #inv_itemIdMap = {v: k for k, v in data_api.itemIdMap.items()}
+    inv_userIdMap = {v: k for k, v in data_layer.userIdMap.items()}
+    inv_itemIdMap = {v: k for k, v in data_layer.itemIdMap.items()}
+    result = dict()
+    for i, ((out, src), major_ind) in enumerate(data_api.iterate_one_epoch_eval(for_inf=True)):
+        inputs = Variable(src.cuda().to_dense())
+        targets_np = out.to_dense().numpy()[0, :]
+        outputs = rencoder_api(inputs).cpu().data.numpy()[0, :]
+        non_zeros = targets_np.nonzero()[0].tolist()
+        major_key = inv_userIdMap[major_ind]
+        for ind in non_zeros:
+            result[inv_itemIdMap[ind]] = outputs[ind]
+    return result
+
+    
 @app.route("/")
 def index():
-    return "Yeah, I highly recommend it"
+    return "Yeah, yeah, I highly recommend it"
+
+
+@app.route("/recommend", methods=['POST'])
+def recommend():
+    if not request.is_json:
+        abort(BAD_REQUEST)
+    dict_query = request.get_json()
+    dict_query = dict((decode_string(k), decode_string(v)) for k, v in dict_query.items())
+    data_api = manage_query(dict_query, data_layer)
+    result = evaluate_model(rencoder_api, data_api, data_layer)
+    print("Result: {}".format(result))
+    return make_response(result, STATUS_OK)
+    #return "{}".format(result)
+
+  
+#Load data and model as global variables
+data_layer = load_train_data(TRAIN)
+rencoder_api = load_recommender(data_layer.vector_dim, HIDDEN, ACTIVATION, DROPOUT, MODEL_PATH)
 
 
 if __name__ == "__main__":
--- Intro_to_Recommendation_Systems/utils.py
+++ Intro_to_Recommendation_Systems/utils.py
@@ -5,6 +5,7 @@ import glob
 import json
 import numpy as np
 import time
+from ast import literal_eval
 
 
 def get_number_processors():
@@ -133,6 +134,54 @@ def _make_directory(directory):
     """Make a directory"""
     if not os.path.isdir(directory):
         os.makedirs(directory)
+        
 
+def decode_string(s):
+    """Convert a string literal to a number or a bool.
+    Parameters:
+        s (str): String
+    Returns:
+        val (str,float,int or bool): Value decoded
+    Examples:
+        >>> decode_string('a')
+        'a'
+        >>> val = decode_string('1.0')
+        >>> type(val)
+        <class 'int'>
+        >>> val
+        1
+        >>> val = decode_string('1')
+        >>> type(val)
+        <class 'int'>
+        >>> val
+        1
+        >>> val = decode_string('1.5')
+        >>> type(val)
+        <class 'float'>
+        >>> val
+        1.5
+        >>> val = decode_string('True')
+        >>> type(val)
+        <class 'bool'>
+        >>> val
+        True
+
+    """
+    if isinstance(s, str):
+        # Does it represent a literal?
+        try:
+            val = literal_eval(s)
+        except:
+            # if it doesn't represent a literal, no conversion is done
+            val = s
+    else:
+        # It's already something other than a string
+        val = s
+    # Is the float actually an int? (i.e. is the float 1.0 ?)
+    if isinstance(val, float):
+        if val.is_integer():
+            return int(val)
+        return val
+    return val
  
     
