9ca86ff19d0cf275f8ef664bc48cd221c2a77d46
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-03-21 15:51:00 +0000

    blokeh fix #72

--- Time_Series_Forecasting_of_Stock_Price/utils.py
+++ Time_Series_Forecasting_of_Stock_Price/utils.py
@@ -7,6 +7,9 @@ import numpy as np
 import time
 from ast import literal_eval
 import matplotlib.pyplot as plt
+from bokeh.plotting import figure
+from bokeh.io import show, export_svgs
+from IPython.display import SVG, display
 
 
 def get_number_processors():
@@ -46,23 +49,28 @@ def plot_series(values, xlabel=None, ylabel=None, color='b', legend=None):
 
     
 def plot_series_prediction(true_values, train_predict, test_predict, time_ahead=1, title=None,
-                           xlabel=None, ylabel=None, color=['g','r','b'], legend=[None,None,None]):
-    pred_trainPlot = np.empty_like(true_values)
-    pred_trainPlot[:, :] = np.nan
-    pred_trainPlot[time_ahead:len(train_predict)+time_ahead, :] = train_predict
+                           xlabel=None, ylabel=None, color=['green','red','blue'], legend=[None,None,None]):    
+    TOOLS = 'pan,wheel_zoom,box_zoom,reset,save,box_select'
+    #x axis
+    xx = np.array(range(true_values.shape[0]))
+    xx1 = np.array(range(time_ahead,len(train_predict)+time_ahead))
+    xx2 = np.array(range(len(train_predict)+(time_ahead*2)+1,len(true_values)-1))
+    
+    #figure
+    p = figure(title=title, tools=TOOLS)
+    p.line(xx, true_values.squeeze(), legend=legend[0], line_color=color[0], line_width=2)
+    p.line(xx1, train_predict.squeeze(), legend=legend[1], line_color=color[1], line_width=1)    
+    p.line(xx2, test_predict.squeeze(), legend=legend[2], line_color=color[2], line_width=1)
+    p.axis[0].axis_label = xlabel
+    p.axis[1].axis_label = ylabel
+    p.legend.location = "top_left"
+    show(p)
+    
+    #save to svg so it is rendered on github
+    p.output_backend = "svg"
+    export_svgs(p, filename=title + '_.svg')
+    display(SVG(title + '_.svg'))
 
-    pred_testPlot = np.empty_like(true_values)
-    pred_testPlot[:, :] = np.nan
-    pred_testPlot[len(train_predict)+(time_ahead*2)+1:len(true_values)-1, :] = test_predict
-
-    plt.plot(true_values, color[0], label=legend[0])
-    plt.plot(pred_trainPlot, color[1], label=legend[1])
-    plt.plot(pred_testPlot, color[2], label=legend[2])
-    plt.legend(loc = 'upper left')
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
-    plt.show()
     
     
     
