b06ff3080f1f593123df387c4c9c88b551d01cbd
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-06-09 22:38:06 +0100

    plot cm

--- Intro_to_Fraud_Detection/utils.py
+++ Intro_to_Fraud_Detection/utils.py
@@ -1,6 +1,8 @@
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score, f1_score, log_loss, precision_score, recall_score
 import numpy as np
+import itertools
+import matplotlib.pyplot as plt
 
 
 def split_train_test(X, y, test_size=0.2):
@@ -93,3 +95,44 @@ def classification_metrics_binary_prob(y_true, y_prob):
 def binarize_prediction(y, threshold=0.5):
     y_pred = np.where(y > threshold, 1, 0)
     return y_pred
+
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    """Plots a confusion matrix.
+    Args:
+        cm (numpy array): The confusion matrix array.
+        classes (list): List wit the classes names.
+        normalize (bool): Flag to normalize data.
+        title (str): Title of the plot.
+        cmap (matplotlib.cm): Matplotlib colormap https://matplotlib.org/api/cm_api.html
+    Examples:
+        >>> import numpy as np
+        >>> a = np.array([[10, 3, 0],[1, 2, 3],[1, 5, 9]])
+        >>> classes = ['cl1', 'cl2', 'cl3']
+        >>> plot_confusion_matrix(a, classes, normalize=False)
+        >>> plot_confusion_matrix(a, classes, normalize=True)
+
+    """
+    cm_max = cm.max()
+    cm_min = cm.min()
+    if cm_min > 0: cm_min = 0
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        cm_max = 1
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    thresh = cm_max / 2.
+    plt.clim(cm_min, cm_max)
+
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i,
+                 round(cm[i, j], 3),  # round to 3 decimals if they are float
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
