c1754df4ddfd0474e1c91aa96695118f499bd486
miguelgfierro <miguelgfierro@users.noreply.github.com>
2018-07-30 07:00:26 +0100

    starting from scratch websockets

--- Intro_to_Fraud_Detection/static/js/frauddetection.js
+++ Intro_to_Fraud_Detection/static/js/frauddetection.js
@@ -1,158 +1,164 @@
 
 $(document).ready(function () {
-// An application can open a connection on multiple namespaces, and
-// Socket.IO will multiplex all those connections on a single
-// physical channel. If you don't care about multiple channels, you
-// can set the namespace to an empty string.
-var namespace = "/fraud";
+    // An application can open a connection on multiple namespaces, and
+    // Socket.IO will multiplex all those connections on a single
+    // physical channel. If you don't care about multiple channels, you
+    // can set the namespace to an empty string.
+    var namespace = "/fraud";
 
-// Connect to the Socket.IO server.
-// The connection URL has the following format:
-//     http[s]://<domain>:<port>[/<namespace>]
-var socket_url = location.protocol + "//" + document.domain + ":" + location.port + namespace;
-var socket = io.connect(socket_url);
-console.log("Connected to " + socket_url);
+    // Connect to the Socket.IO server.
+    // The connection URL has the following format:
+    //     http[s]://<domain>:<port>[/<namespace>]
+    var socket_url = location.protocol + "//" + document.domain + ":" + location.port + namespace;
+    var socket = io.connect(socket_url);
+    console.log("Connected to " + socket_url);
 
-function Location(title, latitude, longitude) {
-    this.title = title;
-    this.latitude = parseFloat(latitude);
-    this.longitude = parseFloat(longitude);
-    this.scale = 0.5;
-    this.zoomLevel = 5;
-}
+    // Handler for health signal
+    socket.on("health_signal", function (msg) {
+        console.log("Response from server: " + msg.data + " (note: " + msg.note + ")");
+    });
 
-function getLA() {
-    var latitude = 34.05;
-    var longitude = -118.24;
-    var title = "Los Angeles";
-    return new Location(title, latitude, longitude);
-}
-
-function getMadrid() {
-    var latitude = 40.4167;
-    var longitude = -3.7033;
-    var title = "Madrid";
-    return new Location(title, latitude, longitude);
-}
-
-function getTokio() {
-    var latitude = 35.6785;
-    var longitude = 139.6823;
-    var title = "Tokyo";
-    return new Location(title, latitude, longitude);
-}
-
-function getNewDelhi() {
-    var latitude = 28.6353;
-    var longitude = 77.2250;
-    var title = "New Delhi";
-    return new Location(title, latitude, longitude);
-}
-
-var mapLocations = [];
-
-// locations for testing
-// mapLocations.push(getLA());
-// mapLocations.push(getMadrid());
-// mapLocations.push(getTokio());
-// mapLocations.push(getNewDelhi());
-
-
-// Based on https://www.amcharts.com/demos/custom-html-elements-map-markers/ 
-var map = AmCharts.makeChart("chartdiv", {
-    "type": "map",
-    "theme": "none",
-    "projection": "miller",
-
-    "imagesSettings": {
-        "rollOverColor": "#089282",
-        "rollOverScale": 3,
-        "selectedScale": 3,
-        "selectedColor": "#089282",
-        "color": "#13564e"
-    },
-
-    "areasSettings": {
-        "unlistedAreasColor": "#222222" /* change color of the map */
-    },
-    "dataProvider": {
-        "map": "worldLow",
-        "images": mapLocations
-    },
-});
-
-console.log(map);
-//map.dataProvider.images.push(getNewDelhi());
-
-
-// Location updated emitted by the server via websockets
-socket.on("map_update", function (msg) {  
-    console.log(msg.title);
-    console.log(msg.latitude);
-    console.log(typeof msg.latitude);
-    newLocation = new Location(msg.title, msg.latitude, msg.longitude);
-    map.dataProvider.images.push(newLocation);
-});
-
-
-
-
-// add events to recalculate map position when the map is moved or zoomed
-map.addListener("positionChanged", updateCustomMarkers);
-
-
-// this function will take current images on the map and create HTML elements for them
-function updateCustomMarkers(event) {
-    // get map object
-    var map = event.chart;
-
-    // go through all of the images
-    for (var x in map.dataProvider.images) {
-        // get MapImage object
-        var image = map.dataProvider.images[x];
-
-        // check if it has corresponding HTML element
-        if ('undefined' == typeof image.externalElement)
-            image.externalElement = createCustomMarker(image);
-
-        // reposition the element accoridng to coordinates
-        var xy = map.coordinatesToStageXY(image.longitude, image.latitude);
-        image.externalElement.style.top = xy.y + 'px';
-        image.externalElement.style.left = xy.x + 'px';
-    }
-}
-
-// this function creates and returns a new marker element
-function createCustomMarker(image) {
-    // create holder
-    var holder = document.createElement('div');
-    holder.className = 'map-marker';
-    holder.title = image.title;
-    holder.style.position = 'absolute';
-
-    // maybe add a link to it?
-    if (undefined != image.url) {
-        holder.onclick = function () {
-            window.location.href = image.url;
-        };
-        holder.className += ' map-clickable';
+    function Location(title, latitude, longitude) {
+        this.title = title;
+        this.latitude = parseFloat(latitude);
+        this.longitude = parseFloat(longitude);
+        this.scale = 0.5;
+        this.zoomLevel = 5;
     }
 
-    // create dot
-    var dot = document.createElement('div');
-    dot.className = 'dot';
-    holder.appendChild(dot);
+    function getLA() {
+        var latitude = 34.05;
+        var longitude = -118.24;
+        var title = "Los Angeles";
+        return new Location(title, latitude, longitude);
+    }
 
-    // create pulse
-    var pulse = document.createElement('div');
-    pulse.className = 'pulse';
-    holder.appendChild(pulse);
+    function getMadrid() {
+        var latitude = 40.4167;
+        var longitude = -3.7033;
+        var title = "Madrid";
+        return new Location(title, latitude, longitude);
+    }
 
-    // append the marker to the map container
-    image.chart.chartDiv.appendChild(holder);
+    function getTokio() {
+        var latitude = 35.6785;
+        var longitude = 139.6823;
+        var title = "Tokyo";
+        return new Location(title, latitude, longitude);
+    }
 
-    return holder;
-}
+    function getNewDelhi() {
+        var latitude = 28.6353;
+        var longitude = 77.2250;
+        var title = "New Delhi";
+        return new Location(title, latitude, longitude);
+    }
+
+    var mapLocations = [];
+
+    // locations for testing
+    // mapLocations.push(getLA());
+    // mapLocations.push(getMadrid());
+    // mapLocations.push(getTokio());
+    // mapLocations.push(getNewDelhi());
+
+
+    // Based on https://www.amcharts.com/demos/custom-html-elements-map-markers/ 
+    // var map = AmCharts.makeChart("chartdiv", {
+    //     "type": "map",
+    //     "theme": "none",
+    //     "projection": "miller",
+
+    //     "imagesSettings": {
+    //         "rollOverColor": "#089282",
+    //         "rollOverScale": 3,
+    //         "selectedScale": 3,
+    //         "selectedColor": "#089282",
+    //         "color": "#13564e"
+    //     },
+
+    //     "areasSettings": {
+    //         "unlistedAreasColor": "#222222" /* change color of the map */
+    //     },
+    //     "dataProvider": {
+    //         "map": "worldLow",
+    //         "images": mapLocations
+    //     },
+    // });
+
+    //console.log(map);
+    //map.dataProvider.images.push(getNewDelhi());
+
+
+    // Location updated emitted by the server via websockets
+    // socket.on("map_update", function (msg) {
+    //     console.log(msg.title);
+    //     console.log(msg.latitude);
+    //     console.log(typeof msg.latitude);
+    //     newLocation = new Location(msg.title, msg.latitude, msg.longitude);
+    //     map.dataProvider.images.push(newLocation);
+    //     map.validateData(); //call to redraw the map with new data
+    // });
+
+
+
+
+    // add events to recalculate map position when the map is moved or zoomed
+    // map.addListener("positionChanged", updateCustomMarkers);
+
+
+    // // this function will take current images on the map and create HTML elements for them
+    // function updateCustomMarkers(event) {
+    //     // get map object
+    //     var map = event.chart;
+
+    //     // go through all of the images
+    //     for (var x in map.dataProvider.images) {
+    //         // get MapImage object
+    //         var image = map.dataProvider.images[x];
+
+    //         // check if it has corresponding HTML element
+    //         if ('undefined' == typeof image.externalElement)
+    //             image.externalElement = createCustomMarker(image);
+
+    //         // reposition the element accoridng to coordinates
+    //         var xy = map.coordinatesToStageXY(image.longitude, image.latitude);
+    //         image.externalElement.style.top = xy.y + 'px';
+    //         image.externalElement.style.left = xy.x + 'px';
+    //     }
+    // }
+
+    // // this function creates and returns a new marker element
+    // function createCustomMarker(image) {
+    //     // create holder
+    //     var holder = document.createElement('div');
+    //     holder.className = 'map-marker';
+    //     holder.title = image.title;
+    //     holder.style.position = 'absolute';
+
+    //     // maybe add a link to it?
+    //     if (undefined != image.url) {
+    //         holder.onclick = function () {
+    //             window.location.href = image.url;
+    //         };
+    //         holder.className += ' map-clickable';
+    //     }
+
+    //     // create dot
+    //     var dot = document.createElement('div');
+    //     dot.className = 'dot';
+    //     holder.appendChild(dot);
+
+    //     // create pulse
+    //     var pulse = document.createElement('div');
+    //     pulse.className = 'pulse';
+    //     holder.appendChild(pulse);
+
+    //     // append the marker to the map container
+    //     image.chart.chartDiv.appendChild(holder);
+
+    //     return holder;
+    // }
 
 
     // Interval function that tests message latency by sending a "ping"
@@ -180,5 +186,9 @@ function createCustomMarker(image) {
         $("#ping-pong").text(Math.round(10 * sum / pingPongTimes.length) / 10);
     });
 
+    // // Handler for health signal
+    // socket.on("health_signal", function (msg) {
+    //     console.log("Response from server: " + msg.data + " (note: " + msg.note + ")");
+    // });
 
 });
