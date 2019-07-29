3e380a3960122accf0f23d2beeeb1c0070a3e623
miguelgfierro <miguelgfierro@users.noreply.github.com>
2019-07-29 22:45:51 +0100

    time series tools

--- Data_Generation/tools.py
+++ Data_Generation/tools.py
@@ -38,3 +38,102 @@ def plot_image(img):
     plt.axis("off")
     plt.show()
     
+    
+def plot_series(x, title=None, ):
+    plt.plot(myX)
+    plt.title(title)
+    plt.axis([0,3600,-1.5,1.5])
+    plt.show()
+    
+
+def DA_Jitter(X, sigma=0.05):
+    myNoise = np.random.normal(loc=0, scale=sigma, size=X.shape)
+    return X+myNoise
+
+
+def DA_Scaling(X, sigma=0.1):
+    scalingFactor = np.random.normal(loc=1.0, scale=sigma, size=(1,X.shape[1])) # shape=(1,3)
+    myNoise = np.matmul(np.ones((X.shape[0],1)), scalingFactor)
+    return X*myNoise
+
+
+## This example using cubic splice is not the best approach to generate random curves. 
+## You can use other aprroaches, e.g., Gaussian process regression, Bezier curve, etc.
+def GenerateRandomCurves(X, sigma=0.2, knot=4):
+    xx = (np.ones((X.shape[1],1))*(np.arange(0,X.shape[0], (X.shape[0]-1)/(knot+1)))).transpose()
+    yy = np.random.normal(loc=1.0, scale=sigma, size=(knot+2, X.shape[1]))
+    x_range = np.arange(X.shape[0])
+    cs_x = CubicSpline(xx[:,0], yy[:,0])
+    cs_y = CubicSpline(xx[:,1], yy[:,1])
+    cs_z = CubicSpline(xx[:,2], yy[:,2])
+    return np.array([cs_x(x_range),cs_y(x_range),cs_z(x_range)]).transpose()
+
+
+def DA_MagWarp(X, sigma):
+    return X * GenerateRandomCurves(X, sigma)
+
+
+def DistortTimesteps(X, sigma=0.2):
+    tt = GenerateRandomCurves(X, sigma) # Regard these samples aroun 1 as time intervals
+    tt_cum = np.cumsum(tt, axis=0)        # Add intervals to make a cumulative graph
+    # Make the last value to have X.shape[0]
+    t_scale = [(X.shape[0]-1)/tt_cum[-1,0],(X.shape[0]-1)/tt_cum[-1,1],(X.shape[0]-1)/tt_cum[-1,2]]
+    tt_cum[:,0] = tt_cum[:,0]*t_scale[0]
+    tt_cum[:,1] = tt_cum[:,1]*t_scale[1]
+    tt_cum[:,2] = tt_cum[:,2]*t_scale[2]
+    return tt_cum
+
+
+def DA_TimeWarp(X, sigma=0.2):
+    tt_new = DistortTimesteps(X, sigma)
+    X_new = np.zeros(X.shape)
+    x_range = np.arange(X.shape[0])
+    X_new[:,0] = np.interp(x_range, tt_new[:,0], X[:,0])
+    X_new[:,1] = np.interp(x_range, tt_new[:,1], X[:,1])
+    X_new[:,2] = np.interp(x_range, tt_new[:,2], X[:,2])
+    return X_new
+
+
+def DA_Rotation(X):
+    axis = np.random.uniform(low=-1, high=1, size=X.shape[1])
+    angle = np.random.uniform(low=-np.pi, high=np.pi)
+    return np.matmul(X , axangle2mat(axis,angle))
+
+
+def DA_Permutation(X, nPerm=4, minSegLength=10):
+    X_new = np.zeros(X.shape)
+    idx = np.random.permutation(nPerm)
+    bWhile = True
+    while bWhile == True:
+        segs = np.zeros(nPerm+1, dtype=int)
+        segs[1:-1] = np.sort(np.random.randint(minSegLength, X.shape[0]-minSegLength, nPerm-1))
+        segs[-1] = X.shape[0]
+        if np.min(segs[1:]-segs[0:-1]) > minSegLength:
+            bWhile = False
+    pp = 0
+    for ii in range(nPerm):
+        x_temp = X[segs[idx[ii]]:segs[idx[ii]+1],:]
+        X_new[pp:pp+len(x_temp),:] = x_temp
+        pp += len(x_temp)
+    return X_new
+
+
+def RandSampleTimesteps(X, nSample=1000):
+    X_new = np.zeros(X.shape)
+    tt = np.zeros((nSample,X.shape[1]), dtype=int)
+    tt[1:-1,0] = np.sort(np.random.randint(1,X.shape[0]-1,nSample-2))
+    tt[1:-1,1] = np.sort(np.random.randint(1,X.shape[0]-1,nSample-2))
+    tt[1:-1,2] = np.sort(np.random.randint(1,X.shape[0]-1,nSample-2))
+    tt[-1,:] = X.shape[0]-1
+    return tt
+
+
+def DA_RandSampling(X, nSample=1000):
+    tt = RandSampleTimesteps(X, nSample)
+    X_new = np.zeros(X.shape)
+    X_new[:,0] = np.interp(np.arange(X.shape[0]), tt[:,0], X[tt[:,0],0])
+    X_new[:,1] = np.interp(np.arange(X.shape[0]), tt[:,1], X[tt[:,1],1])
+    X_new[:,2] = np.interp(np.arange(X.shape[0]), tt[:,2], X[tt[:,2],2])
+    return X_new
+
+
