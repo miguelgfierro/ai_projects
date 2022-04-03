68adb6f9bf0279c67f518339e3af15f083c065e2
miguelgfierro <miguelgfierro@users.noreply.github.com>
2022-04-03 09:42:00 +0200

    trees

--- Grandient_Boosting_Decision_Trees/utils.py
+++ Grandient_Boosting_Decision_Trees/utils.py
@@ -1,12 +1,14 @@
 # Source of part of this code:
 # https://github.com/Azure/fast_retraining/
 # https://github.com/miguelgfierro/codebase/
+# https://jakevdp.github.io/PythonDataScienceHandbook/05.08-random-forests.html
 
 
 import os
 import numpy as np
 import pandas as pd
 from timeit import default_timer
+import matplotlib.pyplot as plt
 from sklearn.metrics import (
     roc_auc_score,
     accuracy_score,
@@ -221,3 +223,35 @@ def classification_metrics_binary_prob(y_true, y_prob):
     m_auc = roc_auc_score(y_true, y_prob)
     report = {"AUC": m_auc}
     return report
+
+
+def visualize_classifier(model, X, y, ax=None, cmap="rainbow"):
+    ax = ax or plt.gca()
+
+    # Plot the training points
+    ax.scatter(
+        X[:, 0], X[:, 1], c=y, s=30, cmap=cmap, clim=(y.min(), y.max()), zorder=3
+    )
+    ax.axis("tight")
+    ax.axis("off")
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    # Predict with the estimator
+    xx, yy = np.meshgrid(np.linspace(*xlim, num=200), np.linspace(*ylim, num=200))
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
+
+    # Create a color plot with the results
+    n_classes = len(np.unique(y))
+    contours = ax.contourf(
+        xx,
+        yy,
+        Z,
+        alpha=0.3,
+        levels=np.arange(n_classes + 1) - 0.5,
+        cmap=cmap,
+        clim=(y.min(), y.max()),
+        zorder=1,
+    )
+
+    ax.set(xlim=xlim, ylim=ylim)
