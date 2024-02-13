71501a6eae791200cb24480e315aa80b9cf39858
miguelgfierro <miguelgfierro@users.noreply.github.com>
2024-02-13 22:51:46 +0100

    gradio

---
+++ Intro_to_Chatbots/bot_openai_gradio.py
@@ -0,0 +1,48 @@
+from openai import OpenAI
+import gradio as gr
+
+from secretos import OPENAI_API_KEY  # https://platform.openai.com/account/api-keys
+
+
+# Set your OpenAI API key
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+
+# Initialize an empty conversation history
+messages = []
+
+# Add a system input
+system_message = "You are a helpful assistant."
+messages.append({"role": "system", "content": system_message})
+
+
+# Define the OpenAI chat function
+def openai_chat(user_input):
+    messages.append({"role": "user", "content": user_input})
+
+    # Call OpenAI's Chat API
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        messages=messages,
+    )
+
+    assistant_reply = response.choices[0].message.content
+    messages.append({"role": "assistant", "content": assistant_reply})
+
+    # Return the assistant's reply
+    return assistant_reply
+
+
+# Create the Gradio interface
+iface = gr.Interface(
+    fn=openai_chat,
+    inputs="text",
+    outputs="text",
+    live=True,
+    title="OpenAI Chat",
+    description="Type a message and click Send to chat with the assistant.",
+)
+
+
+# Launch the Gradio app
+iface.launch(width=600)
