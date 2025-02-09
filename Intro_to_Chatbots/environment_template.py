860c4744db0a2960c769f480733d4a0086e99ff5
miguelgfierro <miguelgfierro@users.noreply.github.com>
2025-02-09 19:49:50 +0100

    environment ignore

---
+++ Intro_to_Chatbots/environment_template.py
@@ -0,0 +1,8 @@
+# Usage
+# $ cp environment_template.py environment.py
+
+# OPENAI
+# https://platform.openai.com/account/api-keys
+OPENAI_API_KEY = "sk-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+# https://platform.openai.com/assistants
+OPENAI_ASSISTANT_ID = "asst_XXXXXXXXXXXXXXXXXXXXXXXX"
